c767becec0cfa525aad1b5889c98b73360d1e7fb
Arthur Frese <arthur.vf@aluno.ifsc.edu.br>
2021-11-19 11:19:04 -0300

    mudança de idioma

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -4,6 +4,7 @@
 @section('content')
 <br><br>
 
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -17,7 +18,7 @@
                         <input type="hidden" name="token" value="{{ $token }}">
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Endereço de e-mail') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Email Address') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
